398b5e97fe9a446ea446b1340db6481f1f28be39
HGermanRocha <german.rocha18@gmail.com>
2017-07-17 22:37:24 -0600

    All controllers updated and middleware started.

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -6,8 +6,9 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
 
-class TagsController extends BaseController
+class TagsController extends Controller
 {
     
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,8 +6,9 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
 
-class UsersController extends BaseController
+class UsersController extends Controller
 {
     
 }
